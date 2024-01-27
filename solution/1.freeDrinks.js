64ec0e9d58ddf2574b60eb52d82822bbc0c24a1e
tanvir243 <travelertanvir@gmail.com>
2024-01-27 20:28:11 +0600

    first task completed

--- solution/1.freeDrinks.js
+++ solution/1.freeDrinks.js
@@ -1,3 +1,7 @@
+/**************************************
+** Task-1
+** Free Drinks
+*/
 var burgerPrice = 550;
 var freeDrinks = burgerPrice > 500 ? "don't need to pay for coke" : "Pay 30 BDT for coke"; 
 
