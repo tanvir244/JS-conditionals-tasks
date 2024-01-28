c7863e9cfb3476fbfde6679e827e17f810349750
tanvir243 <travelertanvir@gmail.com>
2024-01-28 14:25:20 +0600

    ticket fare completed with alternative solution

--- solution/6.ticketFare.js
+++ solution/6.ticketFare.js
@@ -2,14 +2,14 @@
 ** Task-6
 ** Ticket Fare Calculator
 */
+let student = true;
 let age = 65;
-let statuss = 'student';
 let ticketFair = 800;
 let ticketPrice;
 if(age < 10){
     ticketPrice = "Free"; 
 }
-else if (statuss === 'student'){
+else if (student){
     ticketPrice = (ticketFair * 50) / 100;
 }
 else if(age >= 60){
@@ -19,3 +19,9 @@ else{
     ticketPrice = ticketFair;
 }
 console.log(ticketPrice);
+
+// ternary operator
+(age < 10) ? console.log("Free")
+    : (student) ? console.log("50% discount")
+    : (age >= 60) ? console.log("15% discount")
+    : console.log("No discount!")
